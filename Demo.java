1279d7464a6ae005b49031c09548640772032ca2
Jagadisha HM <jagadisha.hm@microfocus.com>
2021-10-22 11:49:34 +0530

    Added Demo file

---
+++ Demo.java
@@ -0,0 +1,11 @@
+public class Demo{
+public static void main(String[] args){
+System.out.println("Main method starts execution");
+addition(10,20);
+System.out.println("Main method ends execution");
+}
+
+public static void addition(int a, int b){
+System.out.println("Addition of two numbers are: "+(a+b));
+}
+}
